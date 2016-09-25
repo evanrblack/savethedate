a1aa75ad149514515d1ab5f032e8ca45aeba53d0
Evan Black <evan@erblack.com>
2016-09-24 23:57:32 -0500

    Add sass to the mix, attempt more with layout

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync . s3://2017savethedate --exclude ".git/*" --exclude "deploy.sh" --acl "public-read"
+aws s3 sync . s3://2017savethedate --exclude ".git/*" --exclude ".sass-cache/*" --exclude "deploy.sh" --exclude "main.scss" --acl "public-read"
