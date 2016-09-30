744ccc37a8c6bf5019e5c0d0df3487ec8dc852ff
Evan Black <evan@erblack.com>
2016-09-29 22:14:33 -0500

    Revise deploy for website s3 name scheme

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-aws s3 sync . s3://2017savethedate \
+aws s3 sync . s3://2017savethe.date \
+    --exclude ".git/*" \
+    --exclude ".gitignore" \
+    --exclude ".sass-cache/*" \
+    --exclude "deploy.sh" \
+    --exclude "main.scss" \
+    --acl "public-read"
+
+aws s3 sync . s3://www.2017savethe.date \
     --exclude ".git/*" \
     --exclude ".gitignore" \
     --exclude ".sass-cache/*" \
