895e119ae81a480493f3dfc4e54d3ce2586991bc
Evan Black <evan@erblack.com>
2016-09-29 23:24:14 -0500

    Fix registry links triggering header link event

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $('.info').hide();
 
 $(function() {
-  $('section a').click(function(e) {
+  $('section h3 a').click(function(e) {
     e.preventDefault();
     $(e.target).closest('section').find('.info').slideToggle();
   });
