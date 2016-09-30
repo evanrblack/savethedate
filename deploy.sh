d7368c67fb06cb852f2f3a391ea90d5e37715fbd
Evan Black <evan@erblack.com>
2016-09-29 22:07:23 -0500

    Complete revamp

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-aws s3 sync . s3://2017savethedate --exclude ".git/*" --exclude ".sass-cache/*" --exclude "deploy.sh" --exclude "main.scss" --acl "public-read"
+aws s3 sync . s3://2017savethedate \
+    --exclude ".git/*" \
+    --exclude ".gitignore" \
+    --exclude ".sass-cache/*" \
+    --exclude "deploy.sh" \
+    --exclude "main.scss" \
+    --acl "public-read"
--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+$('.info').hide();
 
+$(function() {
+  $('section a').click(function(e) {
+    e.preventDefault();
+    $(e.target).closest('section').find('.info').slideToggle();
+  });
+});
